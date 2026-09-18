c527d3cebfc0031981c4896be05a7f8bf0200fa3
Aditya Warekar <adityawarekar1984@gmail.com>
2026-09-18 12:03:20 +0530

    Harden backend security

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,24 @@
 import express from "express";
+import cors from "cors";
+
 import logger from "./middleware/logger.middleware.js";
-import routes from "./routes/index.js"
+import routes from "./routes/index.js";
+import helmet from "helmet";
 
 const app = express();
+
+app.use(helmet());
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 app.use(express.json());
 
 app.use(logger);
-app.use(routes);
 
+app.use(routes);
 
 export default app;
